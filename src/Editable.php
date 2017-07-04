0c13d5d8ee917ff6196636ca85e3248858e65b48
Antonio Ramirez <amigo.cobos@gmail.com>
2017-07-04 20:50:14 +0200

    minor changes

--- src/Editable.php
+++ src/Editable.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @copyright Copyright (c) 2013-2017 2amigOS! Consulting Group LLC
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
@@ -114,9 +114,9 @@ class Editable extends InputWidget
         } else {
             $show = $value;
         }
-        
+
         echo Html::a($show, null, $this->options);
-        
+
         $this->registerClientScript();
     }
 
